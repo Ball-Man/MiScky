fdc9775e29f8f2c5a95f093a690da79736462a70
GiackAloZ <giacomo.aloisi1998@gmail.com>
2017-05-21 19:55:56 +0200

    Added the functionalities to the UIController/__init__.py (Not tested yet)

--- UIController/__init__.py
+++ UIController/__init__.py
@@ -17,96 +17,93 @@ Xlib.InitThreads()
 '''
 
 modules = None
+stop_refreshing = False
 
 def init():
-    global screen
-    global modules
-    modules = []
-    pygame.display.init()
-    pygame.font.init()
-    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+	global screen
+	global modules
+	global stop_refreshing
+	modules = []
+	stop_refreshing = False
+	pygame.display.init()
+	pygame.font.init()
+	screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
 def startPrintTextDiffuse(*args, **kwargs):
-    thread = threading.Thread(target=printTextDiffuse, args=args, kwargs=kwargs)
-    thread.deamon = True
-    thread.start()
-    return thread
+	thread = threading.Thread(target=printTextDiffuse, args=args, kwargs=kwargs)
+	thread.deamon = True
+	thread.start()
+	return thread
 
 def printTextDiffuse(text, coordinates, timeDiffuse):
-    assert(type(text) is Text)
-    assert(type(coordinates) is tuple and len(coordinates) == 2 and all(map(lambda x: type(x) is int, coordinates)))
-    assert(type(timeDiffuse) is int or type(timeDiffuse) is float)
+	assert(type(text) is Text)
+	assert(type(coordinates) is tuple and len(coordinates) == 2 and all(map(lambda x: type(x) is int, coordinates)))
+	assert(type(timeDiffuse) is int or type(timeDiffuse) is float)
 
-    surface = text.render()
-    background = pygame.Surface((surface.get_width(), surface.get_height()))
-    background.fill((0,0,0))
-    background = background.convert()
-    background.blit(surface, (0,0))
+	surface = text.render()
+	background = pygame.Surface((surface.get_width(), surface.get_height()))
+	background.fill((0,0,0))
+	background = background.convert()
+	background.blit(surface, (0,0))
 
-    #surface = surface.convert()
-    atomTime = timeDiffuse / 255
-    
-    for alpha in range(1, 256):
-        background.set_alpha(alpha)
-        screen.blit(background, coordinates)
-        pygame.display.flip()
-        time.sleep(atomTime)
-    
+	#surface = surface.convert()
+	atomTime = timeDiffuse / 255
+	
+	for alpha in range(1, 256):
+		background.set_alpha(alpha)
+		screen.blit(background, coordinates)
+		pygame.display.flip()
+		time.sleep(atomTime)
+	
 def startPrintTextRunning(*args, **kwargs):
-    thread = threading.Thread(target=printTextRunning, args=args, kwargs=kwargs)
-    thread.deamon = True
-    thread.start()
-    return thread
+	thread = threading.Thread(target=printTextRunning, args=args, kwargs=kwargs)
+	thread.deamon = True
+	thread.start()
+	return thread
 
 def printTextRunning(text, coordinates, timeRunning): 
-    assert(type(text) is Text)
-    assert(type(coordinates) is tuple and len(coordinates) == 2 and all(map(lambda x: type(x) is int, coordinates)))
-    assert(type(timeRunning) is int or type(timeRunning) is float)
-        
-    atomTime = timeRunning / len(text.text)
-    tmpText = text.text
-    text.text = ""
-    for a in tmpText:
-        text.text += a
-        if a not in string.whitespace:
-            surface = text.render()
-            screen.blit(surface, coordinates)
-            pygame.display.flip()
-            surface.fill((0,0,0))
-            screen.blit(surface, coordinates)
-            time.sleep(atomTime)
-
-def addModule(moduleType, dimensions, events, position):
-	global modules
-	
-	mod = UIModule(dimensions)
-	if moduleType == 'Calendar':
-		mod = CalendarModule(dimensions, events)
-	elif moduleType == 'Weather':
-		mod = WeatherModule(dimensions, events)
-	elif moduleType == 'Mail':
-		mod = MailModule(dimensions, events)
-	elif moduleType == 'Clock':
-		mod = ClockModule(dimensions)
-	else:
-		mod = UIModule(dimensions)
+	assert(type(text) is Text)
+	assert(type(coordinates) is tuple and len(coordinates) == 2 and all(map(lambda x: type(x) is int, coordinates)))
+	assert(type(timeRunning) is int or type(timeRunning) is float)
 		
-	modules.append((mod, position))
-	return mod.ID
+	atomTime = timeRunning / len(text.text)
+	tmpText = text.text
+	text.text = ""
+	for a in tmpText:
+		text.text += a
+		if a not in string.whitespace:
+			surface = text.render()
+			screen.blit(surface, coordinates)
+			pygame.display.flip()
+			surface.fill((0,0,0))
+			screen.blit(surface, coordinates)
+			time.sleep(atomTime)
 
-def removeModule(ID):
+def startAutoRefresh(seconds):
+	global stop_refreshing
+	async def autoRefresh(s):
+		while not stop_refreshing:
+			refresh()
+			await asyncio.sleep(s)
+		stop_refreshing = False
+	autoRefresh()
+
+def stopAutoRefresh():
+	global stop_refreshing
+	stop_refreshing = True
+
+def addModule(module):
 	global modules
+	modules.append(module)
 
-	modulesTmp = []
-	for mod in modules:
-		if mod[0].ID != ID:
-			modulesTmp.append(mod)
-	modules = modulesTmp
+def removeModule(module):
+	global modules
+	modules.remove(module)
 
 def refresh():
 	screen.fill(BLACK)
 	for mod in modules:
-		screen.blit(mod[0].render(), mod[1])
+		mod.render(screen)
 	pygame.display.flip()
 
 def standby():
