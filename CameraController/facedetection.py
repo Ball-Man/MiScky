3c5e9a87423b5d2f8cc78c689ad2b93cd5b01464
GiackAloZ <giacomo.aloisi1998@gmail.com>
2017-05-24 11:10:56 +0200

    changed resolution

--- CameraController/facedetection.py
+++ CameraController/facedetection.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
 
 def getFaces():
-    p = CameraController.getPhoto((300,300))
+    p = CameraController.getPhoto((320,240))
     image = cv2.imdecode(p, 1)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
