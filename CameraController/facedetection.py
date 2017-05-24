cd2117404ee64e86dac90498b4a3fff91640459c
GiackAloZ <giacomo.aloisi1998@gmail.com>
2017-05-24 09:59:50 +0200

    Now this is actually a package

--- CameraController/facedetection.py
+++ CameraController/facedetection.py
@@ -1,12 +1,14 @@
-import camera
+import CameraController
 import cv2
 
-camera.init()
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
 
 def getFaces():
-    p = camera.getPhoto((300,300))
+    p = CameraController.getPhoto((300,300))
     image = cv2.imdecode(p, 1)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
     return faces
+
+def facePresent():
+	return len(getFaces()) > 0
